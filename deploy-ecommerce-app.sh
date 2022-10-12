4d5383b4acb98152428a0df2d83d25ad4be74ab7
MahmoudAbelaziz22 <mahmoud.abdelaziz.mahmoud22@gmail.com>
2022-10-12 05:52:29 +0200

    adding colored echo messages to deply-ecommerce-app.sh file.

--- deploy-ecommerce-app.sh
+++ deploy-ecommerce-app.sh
@@ -1,21 +1,26 @@
-
 #!/bin/sh
+
+#---------------------------Database Configuration---------------------------------------
+
 #Install FirewallD
+echo "\033[0;32mInstalling FirewallD.....\033[0m"
 sudo yum install -y firewalld
 sudo service firewalld start
 sudo systemctl enable firewalld
 
 #Install MariaDB
+echo "\033[0;32mInstalling MariaDB.....\033[0m"
 sudo yum install -y mariadb-server
 sudo service mariadb start
 sudo systemctl enable mariadb
 
 #Configure firewall for Database
+echo "\033[0;32mConfigureing firewall.....\033[0m"
 sudo firewall-cmd --permanent --zone=public --add-port=3306/tcp
 sudo firewall-cmd --reload
 
 #Configure Database
-
+echo "\033[0;32mConfigureing Database.....\033[0m"
 cat > configure-db.sql -<< EOT
 CREATE DATABASE ecomdb;
 CREATE USER 'ecomuser'@'localhost' IDENTIFIED BY 'ecompassword';
@@ -27,7 +32,7 @@ sudo mysql < configure-db.sql
 
 
 #Create the db-load-script.sql
-
+echo "\033[0;32mCreating the db-load-script.sql.....\033[0m"
 cat > db-load-script.sql <<-EOF
 USE ecomdb;
 CREATE TABLE products (id mediumint(8) unsigned NOT NULL auto_increment,Name varchar(255) default NULL,Price varchar(255) default NULL, ImageUrl varchar(255) default NULL,PRIMARY KEY (id)) AUTO_INCREMENT=1;
@@ -38,23 +43,29 @@ EOF
 
 sudo mysql < db-load-script.sql
 
+#---------------------------Web Server Configuration---------------------------------------
+
 #Install required packages
+echo "\033[0;32mInstalling required web server packages.....\033[0m"
 sudo yum install -y httpd php php-mysqlnd
 sudo firewall-cmd --permanent --zone=public --add-port=80/tcp
 sudo firewall-cmd --reload
 
 #Configure httpd
-
+echo "\033[0;32mConfigureing httpd.....\033[0m"
 sudo sed -i 's/index.html/index.php/g' /etc/httpd/conf/httpd.conf
 
 #Start httpd
+echo "\033[0;32mStarting httpd.....\033[0m"
 sudo service httpd start
 sudo systemctl enable httpd
 
 #Download code
+echo "\033[0;32mDownloadding code.....\033[0m"
 sudo yum install -y git
 sudo git clone https://github.com/kodekloudhub/learning-app-ecommerce.git /var/www/html/
 
-#
+#Update index.php
+echo "\033[0;32mUpdateing index.php.....\033[0m"
 sudo sed -i 's/172.20.1.101/localhost/g' /var/www/html/index.php
 
